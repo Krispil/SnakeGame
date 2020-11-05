9136a3431e28e2d9fab88ca81131424dacf78dc1
Gal Krispil <71902571+Krispil@users.noreply.github.com>
2020-11-05 13:21:01 +0200

    Update game.js

--- game.js
+++ game.js
@@ -5,46 +5,42 @@ import { outsideGrid } from './grid.js'
 let dead = new Audio();
 dead.src = "audio/dead.mp3";
 
-let lastRenderTime = 0 // איפוס זמן פריים
-let gameOver = false //משתנה עזר לפסילה 
-const gameBoard = document.getElementById('game-board') //קישוריות ל html
+let lastRenderTime = 0 
+let gameOver = false 
+const gameBoard = document.getElementById('game-board') 
 
-function main(currentTime) {  //מקבל את הזמן העדכני תמיד 
+function main(currentTime) {  
 
-    if (gameOver) { //בדיקת סוף משחק
+    if (gameOver) {
         dead.play()
         if (confirm('You lost. Press ok to restart.')) {
             window.location = '/'
         }
         return
     }
-    window.requestAnimationFrame(main) //הרצת הפונקציה הראשית בפריימים שאנו נקבע להם  את מספר הפעמיים בשניה
-
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000 // חילוק ב1000 ככה שנעבוד עם שניה 
-    //ונחליט עכשיו כמה פעמים בשניה  נרוץ
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return //עם מספר הפריימים עוד לא הגיע לכמות בשניה שאנו רוצים לא נמשיך קדימה לאיפוס הזמן
-
-    lastRenderTime = currentTime //איפוס הזמן 
-
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    lastRenderTime = currentTime 
     update()
     draw()
 
 }
 
-window.requestAnimationFrame(main) //קריאה ראשונה בשביל להתחיל ריצת משחק
+window.requestAnimationFrame(main) 
 
 function update() {
-    updateSnake() //  יעדכן את מיקום הנחש במטריצה 
-    updateFood() //יעדכן תמיד את מיקום האכול במטריצה 
-    checkDeath() //בדיקת סיום משחק
+    updateSnake() 
+    updateFood() 
+    checkDeath() 
 }
 
 function draw() {
     gameBoard.innerHTML = ''
-    drawSnake(gameBoard) //הצגה ויזואלית 
-    drawFood(gameBoard)//הצגה ויזואלית 
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
 }
 
-function checkDeath() { //בדיקת סיום משחק 
+function checkDeath() { 
     gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
