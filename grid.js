01c09a72eb1f148f643e957b865e097ae8935ffb
Gal Krispil <71902571+Krispil@users.noreply.github.com>
2020-11-05 13:21:48 +0200

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,14 +1,13 @@
-const GRID_SIZE = 21 //מותאם כמובן להגדרת הגודל המקורית ב css
+const GRID_SIZE = 21 
 
-export function randomGridPosition() { //יצירץ מיקום רנדומלי 
-    //נשתמש בזה ביצרית אוכל
+export function randomGridPosition() { 
     return {
         x: Math.floor(Math.random() * GRID_SIZE) + 1,
         y: Math.floor(Math.random() * GRID_SIZE) + 1
     }
 }
 
-export function outsideGrid(position) { //בדיקת  יציאה מתחומי משחק 
+export function outsideGrid(position) { 
     return (
         position.x < 1 || position.x > GRID_SIZE ||
         position.y < 1 || position.y > GRID_SIZE
